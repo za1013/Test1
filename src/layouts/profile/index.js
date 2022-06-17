853fbb51290bb38661aa38932e3356f334c4e912
za1013 <za1013@naver.com>
2022-06-17 09:10:48 +0900

    first

---
+++ src/layouts/profile/index.js
@@ -0,0 +1,57 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import ProfilesList from "examples/Lists/ProfilesList";
+
+// Overview page components
+import Header from "layouts/profile/components/Header";
+// Data
+import profilesListData from "layouts/profile/data/profilesListData";
+
+function Overview() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox mb={2} />
+      <Header>
+        <MDBox mt={5} mb={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
+              <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
+              <Divider orientation="vertical" sx={{ mx: 0 }} />
+            </Grid>
+            <Grid item xs={12} xl={4}>
+              <ProfilesList title="Target users" profiles={profilesListData} shadow={false} />
+            </Grid>
+          </Grid>
+        </MDBox>
+      </Header>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Overview;
